9a7e0318a601acc1cd060fcff6f6d04a5703b543
Shivansh722 <shivanshcha722@gmail.com>
2024-09-19 01:28:11 +0530

    Add basic functionality of the app
    Add textfield and user input, saving chat in local

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,21 +1,39 @@
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from "@/components/ui/button"
- import { useState } from "react"
-
+"use client";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { use, useState } from "react";
 
 export default function ChatPage() {
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<string[]>([]);
 
-    const [message, setMessage] = useState<string>("")
+  const handleSendMessages = () => {
+    setMessages([...messages, message]);
+    setMessage("");
+  }
 
-    return (
-        //put this div at the bottom of the page
-       
-        <div className="grid grid-rows-[20px_1fr_20px] items-top justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Textarea />
-            <Button>Send</Button>
-            </div>
-        
+  const handleSend = (e: any) => {
+    console.log(message);
+    setMessage(e.target.value);
 
+  };
 
-    )
+  
+
+  return (
+    //put this div at the bottom of the page
+
+    <>
+      <div className="grid grid-rows-[20px_1fr_20px] items-top justify-items-center min-h-screen  font-[family-name:var(--font-geist-sans)]">{message}
+
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <div className="grid grid-rows-[20px_1fr_20px] items-top justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <Textarea value={message} onChange={handleSend} />
+        <Button onClick={handleSendMessages}>Send </Button>
+      </div>
+    </>
+  );
 }
